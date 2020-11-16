8b8dcdd7b5aef45cd5218152c6dfeb84efb54479
Seniatical <69202104+Seniatical@users.noreply.github.com>
2020-11-16 20:15:11 +0000

    Update AUTO.py

--- Bot/AUTO.py
+++ Bot/AUTO.py
@@ -1,4 +1,16 @@
+forbidden = ('fuck', 'cock', 'dick', 'fcuk')
+
 message_event = """
-if 'Lol' in message.content:
-    return await ctx.send('Hello')
+try:
+    if message.content.lower() in forbidden:
+        await message.delete()
+        return await message.channel.send('{} that is a forbidden word!'.format(message.author.mention))
+    elif 'discord.gg/' in message.content and message.author.permissions_in(message.channel).manage_messages == False:
+        await message.delete()
+        return await ctx.send('{} You cannot send invite links in the server!'.format(message.author.mention))
+    elif 'https://www.' in message.content and message.author.permissions_in(message.channel).manage_messages == False:
+        await message.delete()
+        return await ctx.send('{} You cannot send website links in the server!'.format(message.author.mention))
+except Exception as error:
+    return await message.channel.send(error)
 """
