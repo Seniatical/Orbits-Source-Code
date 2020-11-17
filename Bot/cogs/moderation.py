c951a30a49b2c85dad540c50325b95c77f25754d
BuildTools <unconfigured@null.spigotmc.org>
2020-11-17 07:51:49 -0800

    continued moderation

--- Bot/cogs/moderation.py
+++ Bot/cogs/moderation.py
@@ -7,16 +7,17 @@ class TEST(commands.Cog):
         self.bot = bot
 
     @commands.command()
+    @commands.has_permissions(kick_members=True)
     async def kick(self, ctx, user: discord.Member = None, *, reason: str = None):
         if user == None:
             return True
         if user == ctx.author:
             return True
 
-        if not reason == None:
-            reason = reason
-        else:
+        if reason == None:
             reason = "Not Valid"
+        else:
+            reason = reason
 
         embed = discord.Embed(
             title="Kick",
@@ -24,6 +25,26 @@ class TEST(commands.Cog):
         )
         embed.set_footer(text="Command done by: <@338999581474029578>")
         await user.kick(reason=reason)
+
+    @commands.command()
+    @commands.has_permissions(ban_members=True)
+    async def ban(self, ctx, user: discord.Member = None, *, reason: str = None):
+        if user == None:
+            return True
+        if user == ctx.author:
+            return True
+
+        if reason == None:
+            reason = "Not Valid"
+        else:
+            reason = reason
+
+        embed = discord.Embed(
+            title="Kick",
+            description=f"{user.mention} has been kicked from {ctx.guild} by {ctx.author} with the reason of: \"{reason}\""
+        )
+        embed.set_footer(text="Command done by: <@338999581474029578>")
+        await user.ban(reason=reason)
 
 
 def setup(bot):
