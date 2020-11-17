40d052ffeb96eefb5c4910844689d6d185f89386
Seniatical <69202104+Seniatical@users.noreply.github.com>
2020-11-17 20:58:43 +0000

    Update AUTO.py
    added stuff

--- Bot/AUTO.py
+++ Bot/AUTO.py
@@ -16,19 +16,39 @@ def profantiy(message):
         return True
     return False
 
-# def is_raid(channel):
-#     x = None
-#     async for message in channel.history(limit=25):
-#         if not x:
-#             x = message
-#             continue
-#         if message.created_at.datetime.datetime.utcnow() - datetime.timedelta(seconds=2) == x.datetime.timedelta(seconds=2):
-#             if x.author.id == message.author.id or x.default_avatar == True and message.author.default_avatar == True:
-#                 if x.author.created_at.datetime.strftime("%Y") == message.author.created_at.datetime.strftime("%Y"):
-#                     return True
-#                 return False
-#             return False
-#         return False
+## def is_raid(channel):
+##     x = None
+##     async for message in channel.history(limit=25):
+##         if not x:
+##             x = message
+##             continue
+##         if message.created_at.datetime.datetime.utcnow() - datetime.timedelta(seconds=2) == x.datetime.timedelta(seconds=2):
+##             if x.author.id == message.author.id or x.default_avatar == True and message.author.default_avatar == True:
+##                 if x.author.created_at.datetime.strftime("%Y") == message.author.created_at.datetime.strftime("%Y"):
+##                     return True
+##                 return False
+##             return False
+##         return False
+
+'''
+Found the error
+fixing soon
+27 => END
+'''
+
+def is_fast_join(guild):
+    for member in guild.members:
+        if member.joined_at.datetime.datetime.utcnow().year == datetime.datetime.utcnow().year:
+            x = guild.members[guild.members.index(member, 0, -1)+1]
+            if x.joined_at.datetime.datetime.utcnow().year == datetime.datetime.utcnow().year:
+                if x.joined_at.datetime.strftime("%j:%a:%H") == member.joined_at.datetime.strftime("%j:%a:%H"):
+                    return True
+        else:
+            continue
+    
+class members:
+    def __init__(self):
+        self.user = {}
     
 message_event = """
 try:
@@ -53,6 +73,7 @@ try:
                 x = self.get_role(role.id)
                 await message.author.remove_roles(x)
             await message.author.add_roles(role)
+            await message.channel.send('{} has automatically been muted for spam pinging.'.format(message.author.mention))
         else:
             perms = discord.Permissions(add_reactions=False, send_messages=False, connect=False)
             role = await ctx.guild.create_role(name='Muted', permissions=perms)
@@ -62,6 +83,7 @@ try:
                 x = self.get_role(role.id)
                 await message.author.remove_roles(x)
             await message.author.add_roles(role)
+            await message.channel.send('{} has automatically been muted for spam pinging.'.format(message.author.mention))
         
 except Exception as error:
     return await message.channel.send(error)
