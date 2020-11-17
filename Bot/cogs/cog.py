eb998546f330a639eb0fedc7960b74a76a7d4d88
D3 <70775520+D3CRYPT360@users.noreply.github.com>
2020-11-17 18:18:36 +0500

    added @commands.command

--- Bot/cogs/cog.py
+++ Bot/cogs/cog.py
@@ -5,21 +5,21 @@ class TEST(commands.Cog):
     def __init__(self, bot):
       self.bot = bot
   
-@bot.command(aliases = ['p'])
-async def poll(self, ctx,*,msg):
-    channel = ctx.channel
-    try:
-        op1 , op2 = msg.split("or")
-        txt = f"React With 1️⃣ for {op1} or 2️⃣ for {op2}"
-    except:
-        await channel.send("Correct syntax: [Choice1] or [Choice2]")
-        return
+    @commands.command(aliases = ['p'])
+    async def poll(self, ctx,*,msg):
+        channel = ctx.channel
+        try:
+            op1 , op2 = msg.split("or")
+            txt = f"React With 1️⃣ for {op1} or 2️⃣ for {op2}"
+        except:
+            await channel.send("Correct syntax: [Choice1] or [Choice2]")
+            return
 
-    embed = discord.Embed(title="🎈POLL TIME", description = txt, color= discord.Color.red())
-    message = await channel.send(embed=embed)
-    await message.add_reaction("1️⃣")
-    await message.add_reaction("2️⃣")
-    await ctx.message.delete()
+        embed = discord.Embed(title="🎈POLL TIME", description = txt, color= discord.Color.red())
+        message = await channel.send(embed=embed)
+        await message.add_reaction("1️⃣")
+        await message.add_reaction("2️⃣")
+        await ctx.message.delete()
 def setup(bot):
   bot.add_cog(TEST(bot))
 
